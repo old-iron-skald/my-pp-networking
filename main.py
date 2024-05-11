b6163acbcad43dd67b307d4cdda40544555813c9
Dryha Andrii <4pa3.14rus@gmail.com>
2024-05-12 00:16:44 +0300

    fix code

--- main.py
+++ main.py
@@ -21,87 +21,96 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps(body if body else {}).encode('utf-8'))
 
     def _pars_body(self):
-        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
-        return json.loads(self.rfile.read(content_length).decode('utf-8'))  # <--- Gets the data itself
+        content_length = int(self.headers['Content-Length'])
+        return json.loads(self.rfile.read(content_length).decode('utf-8'))
 
-    def _reset_users_list(self):
-            USERS_LIST.clear()
-            USERS_LIST.append({
-                "id": 1,
-                "username": "theUser",
-                "firstName": "John",
-                "lastName": "James",
-                "email": "john@email.com",
-                "password": "12345",
-            })
-    
     def do_GET(self):
         if self.path == '/reset':
-            self._reset_users_list()
+            global USERS_LIST
+            USERS_LIST = [
+                {
+                    "id": 1,
+                    "username": "theUser",
+                    "firstName": "John",
+                    "lastName": "James",
+                    "email": "john@email.com",
+                    "password": "12345",
+                }
+            ]
             self._set_response(200, USERS_LIST)
         elif self.path == '/users':
             self._set_response(200, USERS_LIST)
         elif self.path.startswith('/user/'):
             username = self.path.split('/')[-1]
-            for user in USERS_LIST:
-                if user['username'] == username:
-                    self._set_response(200, user)
-                    return
-            self._set_response(400, {"error": "User not found"})
+            user_data = next((user for user in USERS_LIST if user["username"] == username), None)
+            if user_data:
+                self._set_response(200, user_data)
+            else:
+                self._set_response(400, {"error": "User not found"})
+        else:
+            self._set_response(404, {"error": "Not Found"})
 
     def do_POST(self):
         if self.path == '/user':
             try:
-                request_data = self._pars_body()
-                if isinstance(request_data, dict):
-                    if all(key in request_data for key in ["id", "username", "firstName", "lastName", "email", "password"]):
-                        if any(request_data["id"] == user["id"] for user in USERS_LIST):
-                            self._set_response(400, {})
-                        else:
-                            USERS_LIST.append(request_data)
-                            self._set_response(201, request_data)
-                    else:
-                        self._set_response(400, {})
-                elif isinstance(request_data, list):
-                    if all(isinstance(item, dict) and all(key in item for key in ["id", "username", "firstName", "lastName", "email", "password"]) for item in request_data):
-                        if any(item["id"] == user["id"] for user in USERS_LIST for item in request_data):
-                            self._set_response(400, {})
-                        else:
-                            USERS_LIST.extend(request_data)
-                            self._set_response(201, request_data)
-                    else:
-                        self._set_response(400, {})
+                req_body = self._pars_body()
+                if isinstance(req_body, dict):
+                    USERS_LIST.append(req_body)
+                    self._set_response(201, req_body)
+                elif isinstance(req_body, list):
+                    USERS_LIST.extend(req_body)
+                    self._set_response(201, req_body)
                 else:
                     self._set_response(400, {})
-            except json.JSONDecodeError:
+            except Exception as e:
                 self._set_response(400, {})
+        elif self.path == '/user/createWithList':
+            try:
+                req_body = self._pars_body()
+                if isinstance(req_body, list):
+                    ids = [user["id"] for user in USERS_LIST]
+                    if any(user["id"] in ids for user in req_body):
+                        self._set_response(400, {})
+                    else:
+                        USERS_LIST.extend(req_body)
+                        self._set_response(201, req_body)
+                else:
+                    self._set_response(400, {})
+            except Exception as e:
+                self._set_response(400, {})
+        else:
+            self._set_response(404, {"error": "Not Found"})
 
     def do_PUT(self):
         if self.path.startswith('/user/'):
             user_id = int(self.path.split('/')[-1])
             try:
-                request_data = self._pars_body()
-                if all(key in request_data for key in ["username", "firstName", "lastName", "email", "password"]):
-                    for user in USERS_LIST:
-                        if user["id"] == user_id:
-                            user.update(request_data)
-                            self._set_response(200, user)
-                            return
-                    self._set_response(404, {"error": "User not found"})
+                req_body = self._pars_body()
+                if all(key in req_body for key in ("username", "firstName", "lastName", "email", "password")):
+                    index = next((i for i, user in enumerate(USERS_LIST) if user["id"] == user_id), None)
+                    if index is not None:
+                        USERS_LIST[index].update(req_body)
+                        self._set_response(200, USERS_LIST[index])
+                    else:
+                        self._set_response(404, {"error": "User not found"})
                 else:
                     self._set_response(400, {"error": "not valid request data"})
-            except json.JSONDecodeError:
+            except Exception as e:
                 self._set_response(400, {})
+        else:
+            self._set_response(404, {"error": "Not Found"})
 
     def do_DELETE(self):
         if self.path.startswith('/user/'):
             user_id = int(self.path.split('/')[-1])
-            for i, user in enumerate(USERS_LIST):
-                if user["id"] == user_id:
-                    del USERS_LIST[i]
-                    self._set_response(200, {})
-                    return
-            self._set_response(404, {"error": "User not found"})
+            index = next((i for i, user in enumerate(USERS_LIST) if user["id"] == user_id), None)
+            if index is not None:
+                del USERS_LIST[index]
+                self._set_response(200, {})
+            else:
+                self._set_response(404, {"error": "User not found"})
+        else:
+            self._set_response(404, {"error": "Not Found"})
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, host='localhost', port=8000):
     server_address = (host, port)
